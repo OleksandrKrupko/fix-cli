d87999808256a8e5ec57e9b9a26c0dc6b5c81e05
Oleksandr Krupko <oleksandr.krupko@corva.ai>
2022-05-20 19:58:24 +0200

    my-super-fix

---
+++ utils/prompts.js
@@ -0,0 +1,52 @@
+const inquirer = require('inquirer');
+
+module.exports = {
+  askForReleaseBranch,
+  askForForcePush,
+};
+
+async function askForReleaseBranch() {
+  return inquirer
+    .prompt(
+      [
+        {
+          name: 'branch',
+          message: 'Type release branch name',
+        },
+      ],
+      {}
+    )
+    .then(answers => {
+      console.log('you selected the release branch:', answers.branch);
+
+      return answers.branch;
+    })
+    .catch(error => {
+      console.log('something went wrong!', error);
+
+      throw error;
+    });
+}
+
+async function askForForcePush() {
+  return inquirer
+    .prompt(
+      [
+        {
+          name: 'forcePush',
+          type: 'list',
+          message:
+            'Git needs to force push to the fix branch. Do you confirm the force push (Y/N)?',
+          choices: [
+            { name: 'Yes', value: true },
+            { name: 'No', value: false },
+          ],
+          default: 'Yes',
+        },
+      ],
+      {}
+    )
+    .then(answers => {
+      return answers.forcePush;
+    });
+}
